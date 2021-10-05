fcc49f471bdd1269be3a845c13650b4907214b84
gaichi <tomita-gaichi@users.noreply.github.com>
2021-10-05 20:35:43 +0900

    エラーメッセージの表示を修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,2 @@
 module ApplicationHelper
-  def resource
-    :user
- end
- 
- def resource
-    @resource ||= User.new
- end
- 
- def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
- end
 end
