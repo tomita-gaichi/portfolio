aaa0a923801246e675ac168b0c89c12ef4eaccdf
gaichi <tomita-gaichi@users.noreply.github.com>
2021-10-05 20:27:51 +0900

    写真を追加　エラーメッセージの表示を修正

--- app/javascript/home/custom.js
+++ app/javascript/home/custom.js
@@ -1,7 +1,14 @@
 //============================== menu =========================
-const scrollBlue = function () {
-  const blueLeft = document.getElementById("information").getBoundingClientRect().left;
-  const blueTop = document.getElementById("information").getBoundingClientRect().top;
+let actionButton = document.getElementById('actionButton');
+actionButton.onclick = () => target.scrollTop = 400;
+
+let target = document.getElementById('target');
+target.onscroll = () => console.log(target.scrollTop);
+
+window.scrollBlue = function () {
+  console.log('hello');
+  const blueLeft = document.getElementById("map").getBoundingClientRect().left;
+  const blueTop = document.getElementById("map").getBoundingClientRect().top;
   window.scrollTo({
       left: blueLeft,
       top: blueTop,
@@ -11,7 +18,7 @@ const scrollBlue = function () {
 
 //============================== footer =========================
 document.addEventListener('DOMContentLoaded', function(){
-  const pagetopBtn = document.querySelector('#page-top');
+  const pagetopBtn = document.querySelector('page-top');
   if (!pagetopBtn){ return false;}
   pagetopBtn.addEventListener('click', scroll_to_top);
   function scroll_to_top(){
